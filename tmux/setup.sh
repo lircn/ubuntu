9c6c58578585534b1efc379a53b81381b99c2e9a
Li Zheng <lizheng_w5625@tp-link.net>
2016-03-31 10:22:58 +0800

    Update the tmux

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,7 +3,4 @@
 P=`readlink -f $(dirname $0)`
 
 ln -f -s $P/_tmux.conf ~/.tmux.conf
-ln -f -s $P/tmux-powerline/_tmux-powerlinerc ~/.tmux-powerlinerc
-
-# patch fonts
-bash $P/tmux-powerline/fonts/setup.sh
+ln -f -s $P/_tmux-powerlinerc ~/.tmux-powerlinerc
