6fd4508d5dfe075316249e05ab65bdc732cbd14a
Li Zheng <lizheng_w5625@tp-link.net>
2016-04-01 10:25:51 +0800

    Update zsh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,8 +2,12 @@
 
 P=`readlink -f $(dirname $0)`
 
-sodu apt-get install zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-chsh -s /bin/zsh
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    sodu apt-get install zsh
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+    chsh -s /bin/zsh
+fi
+
 ln -f -s $P/lir.zsh-theme ~/.oh-my-zsh/themes/lir.zsh-theme
+ln -f -s $P/lir.zsh ~/.oh-my-zsh/custom/lir.zsh
 ln -f -s $P/_zshrc ~/.zshrc
