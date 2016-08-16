c577fec049d0afcd920aeb4421773020eb40543b
Li Zheng <lizheng_w5625@tp-link.net>
2016-08-16 20:14:41 +0800

    Update powerline

---
+++ powerline/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p ~/.fonts/
+mv PowerlineSymbols.otf ~/.fonts/
+
+fc-cache -vf ~/.fonts/
+
+mkdir -p  ~/.config/fontconfig/conf.d/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+POWERLINE_PATH="~/git/powerline/"
+mkdir -p $POWERLINE_PATH
+cd $POWERLINE_PATH
+git clone https://github.com/powerline/powerline.git
