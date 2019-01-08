853132acee132a2c1d385487836a3be4d6264c24
lir <6007381@qq.com>
2019-01-08 14:44:26 +0800

    Update config

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-P=`readlink -f $(dirname $0)`
+P=`pwd`
 
-# vim files
+# vim file
+# setup dictionary
+echo -n "Setup dictionary? (y/n)"
+read IN
+if [ "$IN" = "y" ] || [ "$IN" = "Y" ]; then
+    #sudo sh -c "apt-get install stardict"
+    #sudo sh -c "apt-get install sdcv"
+    mkdir /tmp/lir_dictionary
+    cp $P/dictionary/* /tmp/lir_dictionary/
+    cd /tmp/lir_dictionary
+    tar -xjf stardict-langdao-ec-gb-2.4.2.tar.bz2
+    rm stardict-langdao-ec-gb-2.4.2.tar.bz2
+    tar -xjf stardict-langdao-ce-gb-2.4.2.tar.bz2
+    rm stardict-langdao-ce-gb-2.4.2.tar.bz2
+    sudo sh -c "mv ./stardict-langdao-* /usr/share/stardict/dic/"
+    cd -
+    rm -rf /tmp/lir_dictionary
+    echo "=== DONE! ==="
+fi
+
+exit 0
+
 echo -n "Backup your original vim files? (y/n)"
 read IN
 if [ "$IN" = "y" ] || [ "$IN" = "Y" ]; then
@@ -19,23 +40,3 @@ ln -s $P/_vimrc $HOME/.vimrc
 
 echo "=== DONE! ==="
 
-# setup dictionary
-echo -n "Setup dictionary? (y/n)"
-read IN
-if [ "$IN" = "y" ] || [ "$IN" = "Y" ]; then
-    sudo sh -c "apt-get install stardict"
-    sudo sh -c "apt-get install sdcv"
-    mkdir /tmp/lir_dictionary
-    cp $P/dictionary/* /tmp/lir_dictionary/
-    cd /tmp/lir_dictionary
-    tar -xjf stardict-langdao-ec-gb-2.4.2.tar.bz2
-    rm stardict-langdao-ec-gb-2.4.2.tar.bz2
-    tar -xjf stardict-langdao-ce-gb-2.4.2.tar.bz2
-    rm stardict-langdao-ce-gb-2.4.2.tar.bz2
-    sudo sh -c "mv -u ./stardict-langdao-* /usr/share/stardict/dic/"
-    cd -
-    rm -rf /tmp/lir_dictionary
-    echo "=== DONE! ==="
-fi
-
-exit 0
